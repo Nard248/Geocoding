80d1004fbcd22d41c14f3c7c2b537d41f67134c9
Narek <meloyann87@gmail.com>
2022-07-26 17:49:36 +0400

    Latitude, Longitude

--- geocoder.py
+++ geocoder.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = 'AIzaSyBzyd4Z64TN7vCvvjGNlzMe8IXKNhrwVW8'
 
-address = '1 hack drive, menlo park, CA'
+address = 'Armenia, Yerevan Komitas Str. 8'
 
 params = {
     'key': API_KEY,
@@ -12,3 +12,9 @@ params = {
 base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
 response = requests.get(base_url, params=params).json()
 print(response)
+
+if response['status'] == 'OK':
+    geometry = response['results'][0]['geometry']
+    lat = geometry['location']['lat']
+    lng = geometry['location']['lng']
+    print(lat, lng)
